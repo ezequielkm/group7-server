317c86ab9911bb35f4d9a68aeb2d3adfe8ca263f
Ezequiel Kneip Maraschin <ezequielkm@live.com>
2023-09-21 21:10:45 -0300

    insert no estoque

--- movimentacao/movimentacao.controller.js
+++ movimentacao/movimentacao.controller.js
@@ -4,11 +4,18 @@ const movimentacaoService = require('./movimentacao.service');
 
 // routes
 router.get('/', getAll);
+router.post('/', addMovimentacao);
 
 module.exports = router;
 
 function getAll(req, res, next) {
     movimentacaoService.getAll()
         .then(movimentacao => res.json(movimentacao))
+        .catch(next);
+}
+
+function addMovimentacao(req, res, next) {
+    movimentacaoService.addMovimentacao(req.body)
+        .then(() => res.json({}))
         .catch(next);
 }
